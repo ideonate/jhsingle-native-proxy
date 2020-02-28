33a444bb622178b091d89395990d229edbaa1eec
Dan Lester <dan@danlester.com>
2020-02-28 09:43:15 +0000

    prefix can be JUPYTERHUB_SERVICE_PREFIX env var, debug flag added

--- jhsingle_native_proxy/main.py
+++ jhsingle_native_proxy/main.py
@@ -3,8 +3,8 @@ from tornado.log import app_log
 from .proxyhandlers import _make_serverproxy_handler
 import click
 import re
+import os
 import logging
-app_log.setLevel(logging.DEBUG)
 
 
 def make_app(destport, prefix, command):
@@ -20,10 +20,19 @@ def make_app(destport, prefix, command):
 @click.command()
 @click.option('--port', default=8888, help='port for the proxy server to listen on')
 @click.option('--destport', default=8500, help='port that the webapp should end up running on')
-@click.option('--prefix', default='/', help='URL prefix to strip before forwarding')
+@click.option('--prefix', default=None, help='URL prefix to strip before forwarding, takes JUPYTERHUB_SERVICE_PREFIX env var if none specified, otherwise /')
 @click.option('--ip', default=None, help='Address to listen on')
+@click.option('--debug/--no-debug', default=False, help='To display debug level logs')
 @click.argument('command', nargs=-1, required=True)
-def run(port, destport, prefix, ip, command):
+def run(port, destport, prefix, ip, debug, command):
+
+    if debug:
+        print('Setting debug')
+        app_log.setLevel(logging.DEBUG)
+
+    if prefix is None:
+        prefix = os.environ.get('JUPYTERHUB_SERVICE_PREFIX', '/')
+
     app = make_app(destport, prefix, list(command))
     app.listen(port, ip)
     print("Starting jhsingle-native-proxy server on address {} port {}, proxying to port {}".format(ip, port, destport))
